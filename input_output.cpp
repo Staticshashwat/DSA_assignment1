430bec022ce3e2f7f71cff73b566b4310c12beca
Staticshashwat <118439884+Staticshashwat@users.noreply.github.com>
2022-12-30 16:21:34 +0530

    Create input_output.cpp

---
+++ input_output.cpp
@@ -0,0 +1,19 @@
+https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{
+  int a,b,c;
+  cin>>a>>b>>c;
+  int s;
+  s=a+b+c;
+  cout<<s;
+}

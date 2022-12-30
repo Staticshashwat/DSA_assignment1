1e1e7950948ae23094aea2d1b8a856edf6275d99
Staticshashwat <118439884+Staticshashwat@users.noreply.github.com>
2022-12-30 16:44:49 +0530

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,21 @@
+https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+   int a;
+   long b;
+   char ch;
+   float f;
+   double d;
+   cin>> a >> b >> ch >> f >> d;
+   cout<<a<<setprecision(9)<<endl;
+    cout<<b<<setprecision(9)<<endl;
+     cout<<ch<<setprecision(9)<<endl;
+      cout<<f<<setprecision(9)<<endl;
+       cout<<d<<setprecision(9)<<endl;    
+    return 0;
+}
